50b9103a439db8fb23e93047ad6f54c38b0b4e85
Michael R. Maletich <maleticm@gmail.com>
2019-09-01 14:08:26 -0500

    Turns lights on and off

--- README.md
+++ README.md
@@ -3,9 +3,16 @@
 ```
 arduino-cli core update-index
 
-# duemilanove
-arduino-cli compile --fqbn arduino:avr:diecimila lights
+arduino-cli core --additional-urls https://adafruit.github.io/arduino-board-index/package_adafruit_index.json install adafruit:samd
+
+arduino-cli lib --additional-urls https://adafruit.github.io/arduino-board-index/package_adafruit_index.json install "Adafruit NeoTrellis M4 Library"
+arduino-cli lib --additional-urls https://adafruit.github.io/arduino-board-index/package_adafruit_index.json install "Adafruit Keypad"
+arduino-cli lib --additional-urls https://adafruit.github.io/arduino-board-index/package_adafruit_index.json install "Adafruit NeoPixel"
+arduino-cli lib --additional-urls https://adafruit.github.io/arduino-board-index/package_adafruit_index.json install "Adafruit DMA neopixel library"
+arduino-cli lib --additional-urls https://adafruit.github.io/arduino-board-index/package_adafruit_index.json install MIDIUSB
+
+arduino-cli compile --fqbn adafruit:samd:adafruit_trellis_m4 lights
 
 arduino-cli board list
-arduino-cli upload -p /dev/cu.usbserial-A9007VOG --fqbn arduino:avr:diecimila lights
+arduino-cli upload -p /dev/cu.usbmodem1D11301 --fqbn adafruit:samd:adafruit_trellis_m4 lights
 ```
--- lights/lights.ino
+++ lights/lights.ino
@@ -1,26 +1,60 @@
+#include "Adafruit_NeoTrellisM4.h"
 
+// The NeoTrellisM4 object is a keypad and neopixel strip subclass
+// that does things like auto-update the NeoPixels and stuff!
+Adafruit_NeoTrellisM4 trellis = Adafruit_NeoTrellisM4();
 
-const int doorPin = 2;
-const int lightsPin = 13;
+boolean *lit_keys;
 
-void setup() {
-  pinMode(doorPin, INPUT);
-  pinMode(lightsPin, OUTPUT);
+void setup(){
+  Serial.begin(115200);
+    
+  trellis.begin();
+  trellis.setBrightness(80);
 
-  digitalWrite(lightsPin, HIGH);
+  Serial.println("toggle keypad test!");
+
+  lit_keys = new boolean[trellis.num_keys()];
   
-  // Open serial communications and wait for port to open:
-  Serial.begin(9600);
-  Serial.println("Started");
+  for (int i=0; i<trellis.num_keys(); i++) {
+    lit_keys[i] = false;
+  }
+}
+  
+void loop() {
+  // put your main code here, to run repeatedly:
+  trellis.tick();
+
+  while (trellis.available()){
+    keypadEvent e = trellis.read();
+    
+    if (e.bit.EVENT == KEY_JUST_PRESSED) {
+      int key = e.bit.KEY;  // shorthand for what was pressed
+      Serial.print(key); Serial.println(" pressed");
+      lit_keys[key] = !lit_keys[key];
+      if (lit_keys[key]) {
+        trellis.setPixelColor(key, Wheel(random(255)));
+      } else {
+        trellis.setPixelColor(key, 0);
+      }      
+    }
+  }
+  
+  delay(10);
 }
 
-void loop() {
-  int doorState = digitalRead(doorPin);
 
-  if (doorState == LOW) {
-    Serial.println("Door Opened");
-    digitalWrite(lightsPin, LOW);
-  } else {
-    digitalWrite(lightsPin, HIGH);
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return trellis.Color(255 - WheelPos * 3, 0, WheelPos * 3);
   }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return trellis.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return trellis.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
 }
