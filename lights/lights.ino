f5ebbfe101ccc266341854494e4fe02a93e67085
Michael R. Maletich <maleticm@gmail.com>
2019-09-03 21:50:41 -0500

    Created onOffMode class

--- lights/lights.ino
+++ lights/lights.ino
@@ -7,62 +7,34 @@ Adafruit_NeoTrellisM4 trellis = Adafruit_NeoTrellisM4();
 enum Mode { menu, onOff };
 
 Mode mode = menu;
-boolean *lit_keys;
 
-void setup(){
-  Serial.begin(115200);
-    
-  trellis.begin();
-  trellis.setBrightness(80);
+class OnOffMode {
 
+public:
+  OnOffMode();
+  void loop();
+  void init();
+
+private:
+  boolean *lit_keys;
+  uint32_t Wheel(byte);
+};
+
+OnOffMode::OnOffMode(void) {
   lit_keys = new boolean[trellis.num_keys()];
 
   for (int i=0; i<trellis.num_keys(); i++) {
     lit_keys[i] = false;
   }
-
-  initMenu();
 }
 
-void initOnOff() {
+void OnOffMode::init() {
   for (int i=0; i<trellis.num_keys(); i++) {
     trellis.setPixelColor(i, 0);
   }
 }
-  
-void loop() {
-  trellis.tick();
 
-  if (trellis.isPressed(0) && trellis.isPressed(7)) {
-    Serial.println("reset mode");
-    initMenu();
-    mode = menu;
-  }
-  switch(mode) {
-    case menu : menuLoop(); break;
-    case onOff : onOffLoop(); break;
-  }
-  
-  delay(10);
-}
-
-void initMenu() {
-  for (int i=0; i<trellis.num_keys(); i++) {
-    trellis.setPixelColor(i, 0);
-  }
-  trellis.setPixelColor(0, trellis.Color(255, 0, 0));
-}
-
-void menuLoop() {
-  keypadEvent e = trellis.read();
-  if ((e.bit.EVENT == KEY_JUST_PRESSED) && (e.bit.KEY == 0)) {
-    Serial.println("Entered onOff mode");
-    initOnOff();
-    mode = onOff;
-  }
-}
-
-void onOffLoop() {
+void OnOffMode::loop() {
   while (trellis.available()){
     keypadEvent e = trellis.read();
     
@@ -79,10 +51,9 @@ void onOffLoop() {
   }
 }
 
-
 // Input a value 0 to 255 to get a color value.
 // The colours are a transition r - g - b - back to r.
-uint32_t Wheel(byte WheelPos) {
+uint32_t OnOffMode::Wheel(byte WheelPos) {
   WheelPos = 255 - WheelPos;
   if(WheelPos < 85) {
     return trellis.Color(255 - WheelPos * 3, 0, WheelPos * 3);
@@ -93,4 +64,47 @@ uint32_t Wheel(byte WheelPos) {
   }
   WheelPos -= 170;
   return trellis.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
+
+OnOffMode onOffMode = OnOffMode();
+
+void setup(){
+  Serial.begin(115200);
+    
+  trellis.begin();
+  trellis.setBrightness(80);
+
+  initMenu();
+}
+  
+void loop() {
+  trellis.tick();
+
+  if (trellis.isPressed(0) && trellis.isPressed(7)) {
+    Serial.println("reset mode");
+    initMenu();
+    mode = menu;
+  }
+  switch(mode) {
+    case menu : menuLoop(); break;
+    case onOff : onOffMode.loop(); break;
+  }
+  
+  delay(10);
+}
+
+void initMenu() {
+  for (int i=0; i<trellis.num_keys(); i++) {
+    trellis.setPixelColor(i, 0);
+  }
+  trellis.setPixelColor(0, trellis.Color(255, 0, 0));
+}
+
+void menuLoop() {
+  keypadEvent e = trellis.read();
+  if ((e.bit.EVENT == KEY_JUST_PRESSED) && (e.bit.KEY == 0)) {
+    Serial.println("Entered onOff mode");
+    onOffMode.init();
+    mode = onOff;
+  }
 }
